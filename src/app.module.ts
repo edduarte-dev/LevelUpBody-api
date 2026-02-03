ab6759cb6f8c3e46afdc00bb6a9d35a873eda4ec
rafaela <rafaelas@genstudents.org>
2026-02-03 18:28:20 -0300

    projeto final

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-
 import { DadosModule } from './dados/dado.module';
 import { UsuarioModule } from './usuarios/usuario.module';
 import { AuthModule } from './auth/auth.module';
 import { ClassificacaoImcModule } from './classificacao/classificacao.module';
-
 import { DevService } from './data/service/dev.service';
 
 @Module({
@@ -19,7 +16,7 @@ import { DevService } from './data/service/dev.service';
     }),
 
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule], // 🔥 ISSO É O FIX
+      imports: [ConfigModule],
       useClass: DevService,
     }),
 
